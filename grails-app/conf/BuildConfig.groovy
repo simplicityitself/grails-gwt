6ad02269afb77aed337edc2bba1be0cb958e33b6
David Dawson <david.dawson@dawsonsystems.com>
2011-10-27 16:07:53 +0100

    Add BuildConfig dep resolution update

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,4 +1,15 @@
 
+grails.project.dependency.resolution = {
+    inherits("global")
+    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    repositories {
+        grailsPlugins()
+        grailsHome()
+        grailsCentral()
+        mavenCentral()
+    }
+}
+
 gwt {
   version="2.4.0"
   parallel=false
