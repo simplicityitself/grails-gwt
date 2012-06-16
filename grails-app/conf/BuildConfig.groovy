a15278a8a85e9b3cf6638edeaabaedc1042d46b3
Rob Purcell <borllecrup@me.com>
2012-06-16 13:14:06 +0100

    Basic upgrade to Grails 2.0.4

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -13,11 +13,10 @@ grails.project.dependency.resolution = {
         //for the release plugin..
         build "org.apache.httpcomponents:httpclient:4.0.3"
         build "org.apache.ivy:ivy:2.2.0"
-        test "org.spockframework:spock-grails-support:0.6-groovy-1.7"
+//        test "org.spockframework:spock-grails-support:0.6-groovy-1.7"
     }
     plugins {
         test(":spock:0.6") { exclude "spock-grails-support" }
-        compile (":new-doc:0.3.2") { exclude "xom" }
     }
 }
 
