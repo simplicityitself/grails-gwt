48576a8397407b37b8b5a84c569e07917849e4fc
BeaVe <donbeave@gmail.com>
2014-06-04 12:00:56 +0800

    Release, REST Client Builder and Resources plugins updated to latest version.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,28 +1,33 @@
-
-grails.project.dependency.resolver = "maven" // or ivy
+grails.project.dependency.resolver = 'maven'
 
 grails.project.dependency.resolution = {
-    inherits("global")
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    inherits('global')
+    log 'warn'
     repositories {
         grailsCentral()
         mavenCentral()
     }
     dependencies {
-      test ("org.spockframework:spock-grails-support:0.7-groovy-2.0") { export = false }
-      build 'com.google.gwt:gwt-user:2.4.0', {
-        export=false
-      }
+        test('org.spockframework:spock-grails-support:0.7-groovy-2.0') {
+            export = false
+        }
+        build 'com.google.gwt:gwt-user:2.4.0', {
+            export = false
+        }
     }
     plugins {
-        test(":spock:0.7") { export = false }
-        build (":release:3.0.0", ':rest-client-builder:1.0.3') { export = false }
-        build ":extended-dependency-manager:0.5.5"
-        runtime ":resources:1.2"
+        test(':spock:0.7') {
+            export = false
+        }
+        build(':release:3.0.1', ':rest-client-builder:2.0.1') {
+            export = false
+        }
+        build ':extended-dependency-manager:0.5.5'
+        runtime ':resources:1.2.8'
     }
 }
 
 grails.release.scm.enabled = false
 
 // Needed to run commands locally that trip over the GWT_HOME check.
-gwt { version = "2.4.0" }
+gwt { version = '2.4.0' }
